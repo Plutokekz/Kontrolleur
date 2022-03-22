3193950eac9f37c3a8780b87770b5e115d52b1b8
BuildTools <unconfigured@null.spigotmc.org>
2022-03-22 22:39:48 +0100

    Add submodule DiplomMaker

---
+++ Diplom/LeagueDiplom.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+import discord
+from datapipelines import NotFoundError
+from discord.ext import commands
+from .Generator.src.FileGenerator import generate_diploma, delete_diploma
+from .Generator.src.LeagueApi import SummonerNotFoundException
+
+
+class DiplomaMaker(commands.Cog):
+
+    @commands.command(name='diplom')
+    async def diplom(self, context, name: str):
+        logging.info(f"Generating Pdf for {name}...")
+        try:
+            path, name = generate_diploma(name)
+            await context.channel.send(file=discord.File(path))
+            await asyncio.sleep(5)
+            delete_diploma(name)
+        except SummonerNotFoundException and NotFoundError:
+            logging.info(f"No account for the name {name}")
+            await context.channel.send(f"Kein Spieler mit dem Namen <{name}> gefunden", delete_after=5)
+        await context.message.delete(delay=10)
+
+
+def setup(client):
+    client.add_cog(DiplomaMaker(client))
